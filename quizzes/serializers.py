e188626e476c936f75906e4dbdb1d7bd9380389f
Daniel <Danielluzius@mail.de>
2026-04-06 17:31:41 +0200

    feat: add QuizSerializer with nested QuestionSerializer

---
+++ quizzes/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Quiz, Question
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    """Serializer for a single quiz question."""
+
+    class Meta:
+        model = Question
+        fields = ['id', 'question_title', 'question_options', 'answer', 'created_at', 'updated_at']
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    """Serializer for a quiz including its nested questions."""
+
+    questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Quiz
+        fields = ['id', 'title', 'description', 'video_url', 'created_at', 'updated_at', 'questions']
