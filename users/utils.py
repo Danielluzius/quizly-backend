19949d165bc665aa59d61f993d66c014cce63525
Daniel <Danielluzius@mail.de>
2026-04-06 17:49:20 +0200

    refactor: extract _set_single_cookie helper to reduce set_auth_cookies line count

--- users/utils.py
+++ users/utils.py
@@ -1,24 +1,28 @@
 from django.conf import settings
 
 
-def set_auth_cookies(response, access_token, refresh_token):
-    """Set access and refresh tokens as HttpOnly cookies on the response."""
-    jwt = settings.SIMPLE_JWT
+def _set_single_cookie(response, key, value, lifetime, jwt):
+    """Set a single HttpOnly cookie on the response."""
     response.set_cookie(
-        key=jwt.get('AUTH_COOKIE', 'access_token'),
-        value=str(access_token),
-        max_age=int(jwt['ACCESS_TOKEN_LIFETIME'].total_seconds()),
+        key=key,
+        value=str(value),
+        max_age=int(lifetime.total_seconds()),
         httponly=jwt.get('AUTH_COOKIE_HTTP_ONLY', True),
         samesite=jwt.get('AUTH_COOKIE_SAMESITE', 'Lax'),
         secure=jwt.get('AUTH_COOKIE_SECURE', False),
     )
-    response.set_cookie(
-        key=jwt.get('AUTH_COOKIE_REFRESH', 'refresh_token'),
-        value=str(refresh_token),
-        max_age=int(jwt['REFRESH_TOKEN_LIFETIME'].total_seconds()),
-        httponly=jwt.get('AUTH_COOKIE_HTTP_ONLY', True),
-        samesite=jwt.get('AUTH_COOKIE_SAMESITE', 'Lax'),
-        secure=jwt.get('AUTH_COOKIE_SECURE', False),
+
+
+def set_auth_cookies(response, access_token, refresh_token):
+    """Set access and refresh tokens as HttpOnly cookies on the response."""
+    jwt = settings.SIMPLE_JWT
+    _set_single_cookie(
+        response, jwt.get('AUTH_COOKIE', 'access_token'),
+        access_token, jwt['ACCESS_TOKEN_LIFETIME'], jwt,
+    )
+    _set_single_cookie(
+        response, jwt.get('AUTH_COOKIE_REFRESH', 'refresh_token'),
+        refresh_token, jwt['REFRESH_TOKEN_LIFETIME'], jwt,
     )
 
 
