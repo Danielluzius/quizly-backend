2a256237d5f70740e1e1d9e4cf1c93b4a381cb8e
Daniel <Danielluzius@mail.de>
2026-04-06 17:24:57 +0200

    feat: register Quiz and Question in admin panel

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Quiz, Question
 
-# Register your models here.
+
+class QuestionInline(admin.TabularInline):
+    """Inline editor for questions within the quiz admin view."""
+
+    model = Question
+    extra = 0
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    """Admin configuration for the Quiz model."""
+
+    list_display = ['title', 'user', 'created_at']
+    inlines = [QuestionInline]
