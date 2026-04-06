bdd7bd1b34077cdb909358676ef2b4c68c97d05c
Daniel <Danielluzius@mail.de>
2026-04-06 17:22:44 +0200

    feat: add CookieJWTAuthentication

---
+++ users/authentication.py
@@ -0,0 +1,14 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.conf import settings
+
+
+class CookieJWTAuthentication(JWTAuthentication):
+    """Read the access token from an HttpOnly cookie instead of the Authorization header."""
+
+    def authenticate(self, request):
+        cookie_name = settings.SIMPLE_JWT.get('AUTH_COOKIE', 'access_token')
+        raw_token = request.COOKIES.get(cookie_name)
+        if raw_token is None:
+            return None
+        validated_token = self.get_validated_token(raw_token)
+        return self.get_user(validated_token), validated_token
